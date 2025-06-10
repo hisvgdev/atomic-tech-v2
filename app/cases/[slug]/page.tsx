ace4b6e699f93d13f5049ef42882bb627d9bd2d1
hisvgdev <kozhemyakokirill@gmail.com>
2025-06-10 21:23:01 +0300

    fix: 🐛 bug

--- app/cases/[slug]/page.tsx
+++ app/cases/[slug]/page.tsx
@@ -1,12 +1,12 @@
+import CaseHistory from '@/components/cases/molecules/CaseHistory'
 import AllProjectsButton from '@/components/Dashboard/molecules/AllProjectsButton'
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { mockProjects } from '@/constants/project.constants'
-import dbIcon from '@/public/assets/icons/database.svg'
-import pythonIcon from '@/public/assets/icons/python.svg'
-import reactIcon from '@/public/assets/icons/react.svg'
+import dbIcon from '@/public/assets/icons/tech/database.svg'
+import pythonIcon from '@/public/assets/icons/tech/pyIcon.svg'
+import reactIcon from '@/public/assets/icons/tech/reactjsIcon.svg'
 import CaseCard from '@/shared/global/CaseCard'
-import { BookOpenIcon, RobotIcon, StarIcon } from '@phosphor-icons/react/dist/ssr'
+import { RobotIcon, StarIcon } from '@phosphor-icons/react/dist/ssr'
 import {
     ArrowRight,
     Calendar,
@@ -29,7 +29,7 @@ export default async function Case({ params }: CaseProps) {
     if (!findCase) return <p>Кейс не найден</p>
 
     return (
-        <main data-dark="false" className="h-full w-full">
+        <main className="h-full w-full">
             <article className="flex flex-col gap-y-16">
                 <header className="flex items-center justify-between">
                     <div className="flex items-center gap-x-10">
@@ -65,7 +65,11 @@ export default async function Case({ params }: CaseProps) {
                     <CategorySection />
                 </section>
 
-                <section aria-labelledby="benefits-heading" className="flex flex-col gap-y-5">
+                <section
+                    data-dark="false"
+                    aria-labelledby="benefits-heading"
+                    className="flex flex-col gap-y-5"
+                >
                     <h2 id="benefits-heading" className="font-bold text-7xl">
                         Мы достигли
                     </h2>
@@ -93,102 +97,13 @@ export default async function Case({ params }: CaseProps) {
                     </div>
                 </section>
 
-                <section className="w-full h-full bg-black rounded-[3.125rem] p-10">
-                    <div className="flex items-start justify-between">
-                        <div className="flex flex-col gap-y-9 max-w-4xl">
-                            <h2 className="text-white font-bold text-5xl">История проекта</h2>
-                            <div className="flex flex-col gap-y-12 text-white/50 font-medium text-base">
-                                <p>
-                                    Сейчас Python развивает и поддерживает большая команда
-                                    разработчиков по всему миру. В 2001 году они объединились и
-                                    создали некоммерческую организацию Python Software Foundation
-                                    (PSF). Эта организация занимается продвижением языка,
-                                    разрабатывает и выпускает новые версии, исправляет ошибки и
-                                    улучшает функциональность.
-                                </p>
-                                <p>
-                                    Основатель языка Гвидо ван Россум за это время успел поработать
-                                    в Google, Dropbox, а в 2020 году присоединился к команде
-                                    Microsoft. В июле 2018 года он объявил, что уходит с поста
-                                    «пожизненного доброжелательного диктатора» Python. Этот термин
-                                    используется в контексте разработки свободного ПО. Так называют
-                                    основателя проекта, который обладает правом принимать
-                                    окончательные решения.
-                                </p>
-                                <p>
-                                    После отхода от дел Россума все права на Python принадлежат
-                                    организации Python (Software Foundation). Она управляет
-                                    лицензиями и следит за тем, чтобы язык оставался открытым для
-                                    использования.
-                                </p>
-                                <p>
-                                    Основные особенности Python Python обладает рядом характеристик
-                                    и особенностей, которые делают его одним из самых популярных
-                                    языков программирования. Перечислим основные из них.
-                                </p>
-                                <p>
-                                    Простота синтаксиса. Язык программирования Python стал популярен
-                                    благодаря своей простоте и чистоте кода. Его легко освоить
-                                    новичкам и даже тем, кто далек от программирования. Например,
-                                    этот язык могут использовать дизайнеры, работающие с трехмерной
-                                    графикой, аналитики данных и тестировщики.
-                                </p>
+                <CaseHistory />
 
-                                <p>
-                                    Python обладает рядом характеристик и особенностей, которые
-                                    делают его одним из самых популярных языков программирования.
-                                    Перечислим основные из них.
-                                </p>
-                                <p>
-                                    Простота синтаксиса. Язык программирования Python стал популярен
-                                    благодаря своей простоте и чистоте кода. Его легко освоить
-                                    новичкам и даже тем, кто далек от программирования. Например,
-                                    этот язык могут использовать дизайнеры, работающие с трехмерной
-                                    графикой, аналитики данных и тестировщики.
-                                </p>
-                            </div>
-                        </div>
-
-                        <aside data-dark="true" className="max-w-sm grow flex flex-col gap-y-6">
-                            <button
-                                type="button"
-                                className="border border-white py-5 rounded-full bg-transparent cursor-pointer text-white hover:bg-transparent flex justify-between px-6"
-                            >
-                                <div className="flex items-center gap-x-4">
-                                    <BookOpenIcon size={24} weight="fill" />
-                                    <span>Блог по тематике</span>
-                                </div>
-                                <ArrowRight />
-                            </button>
-
-                            {[1, 2].map((_, i) => (
-                                <Card
-                                    key={i}
-                                    className="border-none shadow-none h-96 bg-[#1D1D1D] rounded-4xl flex flex-col justify-between"
-                                >
-                                    <CardHeader>
-                                        <CardTitle>
-                                            <div className="bg-black w-full rounded-2xl h-48" />
-                                        </CardTitle>
-                                    </CardHeader>
-                                    <CardContent>
-                                        <h4 className="font-semibold text-white">
-                                            Wordpress — это быстро? Но что если мы расскажем вам о
-                                            новых.
-                                        </h4>
-                                    </CardContent>
-                                    <CardFooter>
-                                        <time className="text-gray-300 font-light text-sm leading-6">
-                                            2025-06-08
-                                        </time>
-                                    </CardFooter>
-                                </Card>
-                            ))}
-                        </aside>
-                    </div>
-                </section>
-
-                <section aria-labelledby="more-cases-heading" className="flex flex-col gap-y-6">
+                <section
+                    data-dark="false"
+                    aria-labelledby="more-cases-heading"
+                    className="flex flex-col gap-y-6"
+                >
                     <h2 id="more-cases-heading" className="font-bold text-7xl">
                         Больше кейсов
                     </h2>
@@ -199,9 +114,9 @@ export default async function Case({ params }: CaseProps) {
                     </div>
                 </section>
 
-                <footer className="mb-1">
+                <div className="mb-1">
                     <AllProjectsButton />
-                </footer>
+                </div>
             </article>
         </main>
     )
